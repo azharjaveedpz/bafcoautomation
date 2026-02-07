e272ae10e5ad95eaa73a7cc8f8c8759676cd07ec
Mac <>
2026-02-07 09:24:39 +0530

    added automation script for new cro creation

--- tests/export.login.spec.ts
+++ tests/export.login.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/ui/LoginPage';
+import { LoginPage } from '../pages/ui/login.page';
 import { loginData } from '../data/ui/login.data';
 
 test('UI Login Test - BAFCO (Export)', async ({ page }) => {
